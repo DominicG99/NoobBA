cc6afd91f149c9cd8410bf5ead8ee0957daf2d4a
Dominic Ginter <dominicginter@Dominics-Laptop.nv.charter.com>
2024-05-31 22:10:15 -0700

    set up boilerplate

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    res.send("YOO");
+});
+
+app.listen(6000, ()=>
+{
+    console.log("Listening on port 6000");
+});
