34e09bc7e54fc102e04ba337ec85b5b4543a7a33
Dominic Ginter <dominicginter@Dominics-Laptop.local>
2024-06-04 16:04:09 -0700

    added file for activePlayers only.

--- server/src/scraper/fetchAllPlayers.ts
+++ server/src/scraper/fetchAllPlayers.ts
@@ -2,7 +2,8 @@ import cheerio from "cheerio";
 import fs from 'fs';
 export async function fetchAllPlayers(): Promise<string[]>
 {
-    let alphabet: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "u", "v", "w", "y", "z"];
+    let alphabet: string[] = ["q", "r", "s", "t", "u", "v", "w", "y", "z"];//["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "p", 
+
     let baseURI: string = "https://www.basketball-reference.com/players/";
     let allPlayers = [];
     for (let i = 0; i < alphabet.length; i++)
@@ -12,19 +13,19 @@ export async function fetchAllPlayers(): Promise<string[]>
         let $ = cheerio.load(respHTML);
         let inActivePlayerNames = $('th > a');
         let activePlayerNames = $('th > strong > a');
-        for (let j = 0; j < inActivePlayerNames.length; j++)
-        {
-            let name = $(inActivePlayerNames[j]).text();
-            allPlayers.push(name);
-        }
+        // for (let j = 0; j < inActivePlayerNames.length; j++)
+        // {
+        //     let name = $(inActivePlayerNames[j]).text();
+        //     allPlayers.push(name);
+        // }
         for (let j = 0; j < activePlayerNames.length; j++)
         {
             let name = $(activePlayerNames[j]).text();
             allPlayers.push(name);
         }
     }
-    await fs.writeFileSync('players.json', JSON.stringify(allPlayers));
+    await fs.writeFileSync('activePlayers2.json', JSON.stringify(allPlayers));
     return allPlayers;
 }
 
-//fetchAllPlayers();
+fetchAllPlayers();
