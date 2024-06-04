0c766851b42d7ded7197f3c5c1e5e2154d5af01b
Dominic Ginter <dominicginter@Dominics-Laptop.local>
2024-06-04 15:42:27 -0700

    added more scrape files and got getPlayer.ts working

---
+++ server/src/scraper/gatherAllPlayerInfo.ts
@@ -0,0 +1,19 @@
+import { allPlayers } from './players.js';
+
+function gatherFivePlayerInfos(players: string[])
+{
+    for (let i = 0; i < players.length; i++)
+    {
+
+    }
+}
+
+for (let i = 0; i < allPlayers.length; i++)
+{
+    //2 minute per offset so we don't get banned for scraping... 
+    let timeoutOffset = 120000;
+
+    // setTimeout(()=> {
+
+    // }, timeoutOffset * i);
+}
--- server/src/scraper/getPlayer.ts
+++ server/src/scraper/getPlayer.ts
@@ -1,11 +1,28 @@
 import cheerio from "cheerio";
 import { fetchPlayerPage } from "./fetchPlayerPage.js";
-import { allPlayers } from './players.js';
+import { grabPlayerSats } from "./grabPlayerStats.js";
+import { grabCareerStats } from "./grabCareerStats.js";
 
 async function getPlayer(playerName: string)
 {
     try
     {
+        let playerObj = {
+            name: playerName,
+            accolades: undefined,
+            draftYear: undefined,
+            draftPick: undefined,
+            draftTeam: undefined,
+            shootingHand: undefined,
+            college: undefined,
+            birthplace: undefined,
+            birthdate: undefined,
+            debut: undefined,
+            height: undefined,
+            weight: undefined,
+            careerLength: -1,
+        };
+
         const firstResultHTML = await fetchPlayerPage(playerName);
         let $ = cheerio.load(firstResultHTML);
         const name = $('#meta h1').text().trim();
@@ -17,6 +34,51 @@ async function getPlayer(playerName: string)
             );
         }
 
+        // Grab the player's accolades
+        const accolades: string[] = [];
+        let championships = 0;
+        let mvps = 0;
+        let allStars = 0;
+        const accoladeList = $('#bling > li');
+
+        accoladeList.each((index, accolade) =>
+        {
+            const accoladeElement = $(accolade);
+            const accoladeText: string | null = $(accolade).text();
+
+            if (/NBA Champ$/.test(accoladeText))
+            {
+                // If it doesn't have an x in it, it's 1 time
+                if (!/x/.test(accoladeText))
+                {
+                    championships = 1;
+                } else
+                {
+                    championships = parseInt(accoladeText.match(/^\d+/)![0]);
+                }
+            } else if (/^\d{4}-\d{2} MVP$|^\d+x MVP$/.test(accoladeText))
+            {
+                // This RegExonly matches MVPs. Not WCF MVPs or ASG or anything
+                // And here the logic is very similar to the championships
+                if (!/x/.test(accoladeText))
+                {
+                    mvps = 1;
+                } else
+                {
+                    mvps = parseInt(accoladeText.match(/^\d+/)![0]);
+                }
+            } else if (/^\d+x All Star$/.test(accoladeText))
+            {
+                // Here they will all have x's, so just get the leading number
+                const regexResult: RegExpMatchArray | null = accoladeText.match(/^\d+/);
+                if (regexResult != null)
+                {
+                    allStars = parseInt(regexResult[0]);
+                }
+            }
+            accolades.push(accoladeElement.text());
+        });
+
         // Now fetch bio info
         let playerInfoContainer = $('#meta div:nth-child(2)');
         if ($('#meta > .nothumb').length > 0)
@@ -24,20 +86,164 @@ async function getPlayer(playerName: string)
             // User has no thumbnail, the player info is in a different place.
             playerInfoContainer = $('#meta > .nothumb');
         }
-        const nicknames = [];
+        const nicknames: string[] = [];
         let draftPick = -1;
         let draftYear = -1;
         let draftTeam = '';
-        const positions = [];
+        const positions: string[] | null = [];
         let shootingHand = '';
         let college = '';
         let birthplace = '';
-        let birthdate = '';
-        let debut = '';
+        let birthdate: any;
+        let debut: any;
         let height = '';
         let weight = -1;
         let careerLength = -1;
         const playerInfoElements = playerInfoContainer.find('p');
+        playerInfoElements.each((index, element) =>
+        {
+            const elementData = $(element);
+            const elementText = $(element).text().trim();
+            if (/^\(.*\)$/.test(elementText))
+            {
+                // Nickname check
+                const nicknameList = elementText;
+                const nicknameArr = nicknameList.replace(/[()\n]/g, '').split(',');
+                for (let i = 0; i < nicknameArr.length; i++)
+                {
+                    nicknames.push(nicknameArr[i].trim());
+                }
+            }
+            else if (/^Draft/.test(elementText))
+            {
+                // Draft check
+                // This returns the number occurences in the draft string
+                draftPick = parseInt(elementText.match(/\d+/g)![2]);
+                draftYear = parseInt(elementText.match(/\d+/g)![3]);
+                draftTeam = elementData.find('a:nth-child(2)').text().trim();
+            }
+            else if (/^Position/.test(elementText))
+            {
+                // Position and handedness check
+                positions.push(...elementText.match(/Guard|Center|Forward|Small Forward|Point Guard|Shooting Guard|Power Forward/g)!);
+                shootingHand = elementText.match(/Right|Left/g)![0];
+            }
+            else if (/^College/.test(elementText))
+            {
+                // College check
+                college = elementText.slice(9).trim();
+            }
+            else if (/^Born/.test(elementText))
+            {
+                // Born info check
+                birthplace = elementData
+                    .find('span:nth-child(3)')
+                    .text()
+                    .trim()
+                    .slice(3);
+                birthdate = new Date(elementData.find('span:nth-child(2)').text().trim());
+            }
+            else if (/^NBA Debut/.test(elementText))
+            {
+                debut = new Date(elementText.slice(11).trim());
+            }
+            else if (/kg\)$/.test(elementText))
+            {
+                // Height and weight check
+                height = elementText.match(/\d-\d+/)![0];
+                weight = parseInt(elementText.match(/\d+/g)![2]);
+            }
+            else if (/^Career Length:/.test(elementText))
+            {
+                // Grab career length if they're retired
+                careerLength = parseInt(elementText.match(/\d+/)![0]);
+            }
+        });
+
+        const statContainer = $('#div_per_game tbody > tr');
+
+        let stats: any = grabPlayerSats(statContainer, $);
+
+        // Now get the career stats and load them
+        const careerStatsRow = $('#div_per_game tfoot > tr').first();
+        stats.career = grabCareerStats(careerStatsRow, $);
+
+        // Finally, get the player's NBA.com id and headshot
+        let picture = '';
+        let playerID = $('#div_stats-nba-com > div > a:nth-child(1)').attr('href');
+        // Null check for player that are retired or otherwise missing this link
+        if (playerID)
+        {
+            playerID = playerID.match(/\d+/)![0];
+            picture = `https://cdn.nba.com/headshots/nba/latest/1040x760/${playerID}.png`;
+        } else
+        {
+            playerID = '';
+        }
+
+        // Grab the jersey numbers and put them in an array
+        const jerseyContainer = $('.uni_holder .jersey text');
+
+        const jerseyNumbers: any = [];
+
+        jerseyContainer.each((index, element) =>
+        {
+            const elementText = $(element).text();
+
+            if (!jerseyNumbers.includes(parseInt(elementText)))
+            {
+                jerseyNumbers.push(parseInt(elementText));
+            }
+        });
+
+        // Lastly, get the player's career earnings and most recent
+        const FAQs = $('#div_faq h3, #div_faq p');
+
+        let recentSalary = -1;
+        let careerEarnings = -1;
+
+        FAQs.each((index, element) =>
+        {
+            const elementText = $(element).text();
+
+            if (/^How much does .* make\?/.test(elementText))
+            {
+                recentSalary = parseInt(
+                    $(element).next().text().match(/[\d,]+/)![0].replace(/,/g, ''));
+            }
+            else if (/^What is .* net worth\?/.test(elementText))
+            {
+                careerEarnings = parseInt($(element).next().text().match(/[\d,]+/)![0].replace(/,/g, ''));
+            }
+        });
+        const player = {
+            name,
+            picture,
+            jerseyNumbers,
+            positions,
+            height,
+            weight,
+            nicknames,
+            championships,
+            mvps,
+            allStars,
+            accolades,
+            stats,
+            shootingHand,
+            college,
+            birthplace,
+            birthdate,
+            draftPick,
+            draftYear,
+            draftTeam,
+            debut,
+            careerLength,
+            recentSalary,
+            careerEarnings,
+        };
+
+        return JSON.stringify(player);
+
     }
     catch (err)
     {
